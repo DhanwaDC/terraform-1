664245f6c8ed417e327553cceb36e1607dbd2c3b
Dhanwa <dhanuradc@gmail.com>
2025-10-02 15:30:12 +0530

    added sg rule

--- provider.tf
+++ provider.tf
@@ -6,17 +6,17 @@ terraform {
     }
   }
   backend "s3" {
-    bucket="devsecops-terra-1-practise"
-    key="devopsterra.tfstate"
-    region="us-east-1"
+    bucket = "devsecops-terra-1-practise"
+    key    = "devopsterra.tfstate"
+    region = "us-east-1"
   }
-  
+
 }
 
 provider "aws" {
- 
+
   # You might also configure authentication methods here,
   # but often these are handled by environment variables or AWS CLI configuration.
-}     
+}
 
 
